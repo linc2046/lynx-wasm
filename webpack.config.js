39afa295513db111dbd11e88df6dcd2a13bafd2e
linc2046 <linc2046@gmail.com>
2022-09-21 18:02:47 +0800

    init wasm repo

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+const dist = path.resolve(__dirname, "dist");
+
+module.exports = {
+  experiments: {
+    // webpack 5 WebAssembly is not enabled by default and flagged as experimental feature.
+    syncWebAssembly: true,
+  },
+  mode: "production",
+  entry: {
+    index: "./js/index.js"
+  },
+  output: {
+    path: dist,
+    filename: "[name].js"
+  },
+  devServer: {
+    contentBase: dist,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "pkg", to: "static" },
+      ]
+    }),
+
+    new WasmPackPlugin({
+      crateDirectory: __dirname,
+    }),
+  ]
+};
